5f8f43ba35f75aaf3e544fe1153dfe9ad1c49e9b
Doğukan <10725670+Dogukan189@users.noreply.github.com>
2019-08-29 13:52:04 +0300

    Added strideX and strideY

--- modules/maxpool_1.py
+++ modules/maxpool_1.py
@@ -3,26 +3,28 @@ import numpy as np
 import math
 
 class simplemaxpool():
-  def __init__(self,image,stride):
-      self.image = cv2.imread(image)
-      self.stride = stride
+  
+  def __init__(self,image,strides=(3,3)):
+    self.image = cv2.imread(image)
+    self.strideX = strides[0]
+    self.strideY = strides[1]
+    
 
   def isInBounds(self,x,y,arr):
-      try:
-          a = arr[x][y]
-      except:
-          return False
-      return True
-    
+    try:
+        a = arr[x][y]
+    except:
+        return False
+    return True
   def max_pool_area(self,x,y,arr):
-      q = []
-      for i in range(self.stride):
-        for j in range(self.stride):
-          if self.isInBounds(i+x,j+y,arr):
-              q.append(arr[i+x][j+y])
-          else:
-              continue
-      return np.amax(q)        
+    q = []
+    for i in range(self.strideX):
+      for j in range(self.strideY):
+        if self.isInBounds(i+x,j+y,arr):
+          q.append(arr[i+x][j+y])
+        else:
+          continue
+    return np.amax(q)        
 
   def MaxPool2D(self):      
     height = self.image.shape[0]
@@ -32,16 +34,16 @@ class simplemaxpool():
     green = self.image[:,:,1]
     blue = self.image[:,:,2]
     
-    newHeight = math.ceil(height / self.stride)
-    newWidth = math.ceil(width / self.stride)
+    newHeight = math.ceil(height / self.strideX)
+    newWidth = math.ceil(width / self.strideY)
 
     maximg = np.zeros((newHeight,newWidth,3),dtype=np.uint8)
-    for k in range(0,height,self.stride):
-        for l in range(0,width,self.stride):
-            newk = int(k / self.stride)
-            newl = int(l / self.stride)
-            maximg[newk][newl][0] = self.max_pool_area(k,l,red)
-            maximg[newk][newl][1] = self.max_pool_area(k,l,green)
-            maximg[newk][newl][2] = self.max_pool_area(k,l,blue)
-
+    for k in range(0,height,self.strideX):
+      for l in range(0,width,self.strideY):
+        newk = int(k / self.strideX)
+        newl = int(l / self.strideY)
+        maximg[newk][newl][0] = self.max_pool_area(k,l,red)
+        maximg[newk][newl][1] = self.max_pool_area(k,l,green)
+        maximg[newk][newl][2] = self.max_pool_area(k,l,blue)
+        
     return maximg
