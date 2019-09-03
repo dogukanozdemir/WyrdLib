3482032a3da6651d2d1af7d97488277df0c018f8
Doğukan <10725670+Dogukan189@users.noreply.github.com>
2019-09-04 00:46:53 +0300

    Update ConvFilter.py

--- modules/ConvFilter.py
+++ modules/ConvFilter.py
@@ -63,11 +63,6 @@ class ConvFilter():
   
   def __init__(self,verbose=1):
     self.verbose = verbose
-  
-  def pad_with(vector, pad_width, iaxis, kwargs):
-    pad_value = kwargs.get('padder', 0)
-    vector[:pad_width[0]] = pad_value
-    vector[-pad_width[1]:] = pad_value
 
   def isInBounds(self,x,y,arr):
     if x >= 0 and y >= 0:
@@ -80,35 +75,21 @@ class ConvFilter():
       return False
     
   def kernel_area(self,x,y,arr):
-    kernel_res = 0
-    for i in range(self.kernelh):
-      for j in range(self.kernelw):
-        if self.isInBounds(i+x-1,j+y-1,arr):
-          kernel_res += arr[i+x-1][j+y-1] * self.kernel_matrix[i][j]
-        else:
-          continue
-    if kernel_res < 0:
-      kernel_res = 0
-    elif kernel_res > 255:
-      kernel_res = 255
-      
-    return kernel_res
-  
-
-  def new_kernel(self,x,y,arr):
     startw = x-1
     starth = y-1 
     if startw < 0:
       startw = 0
     if starth < 0:
       starth = 0
-
     image_kernel = arr[startw:x+self.kernelw-1,starth:y+self.kernelh-1]
-    return sum(sum(image_kernel * self.kernel_matrix))
-  
-  
+    kernel_res = sum(sum(image_kernel * self.kernel_matrix))
+    
+    if kernel_res < 0:
+      kernel_res = 0
+    elif kernel_res > 255:
+      kernel_res = 255
+
   def transform(self,image,ktype):
-    """padding = 'same' or 'valid' """
     start = time.time()
     if ktype not in self.__class__.filter_dict:
       raise TypeError("The filter '" + str(ktype) + "' does not exist.")
@@ -117,17 +98,20 @@ class ConvFilter():
       self.kernelh = len(self.kernel_matrix[0])
       self.kernelw = len(self.kernel_matrix[1])
       
-    image = np.pad(image,self.kernel_matrix.shape[0], self.pad_with)
-
     red = image[:,:,0]
     green = image[:,:,1]
     blue = image[:,:,2]
     
+    red = np.pad(red, pad_width=1, mode='constant', constant_values=0)
+    green = np.pad(green, pad_width=1, mode='constant', constant_values=0)
+    blue = np.pad(blue, pad_width=1, mode='constant', constant_values=0)
+
+    
     height = image.shape[0]
     width = image.shape[1]
     
-    for k in range(height):
-      for l in range(width):
+    for k in range(1,height-1):
+      for l in range(1,width-1):
         image[k][l][0] = self.new_kernel(k,l,red)
         image[k][l][1] = self.new_kernel(k,l,green)
         image[k][l][2] = self.new_kernel(k,l,blue)
@@ -136,4 +120,4 @@ class ConvFilter():
       print("It took {0} for the filter '{1}' to complete transformation"
             .format(datetime.timedelta(seconds=end-start),
                     ktype))           
-    return image
+    return image[1:-1, 1:-1]
